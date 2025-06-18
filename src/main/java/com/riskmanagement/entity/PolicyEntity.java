6d4d7b46863f6f0056e57914ed781fd08cd0558e
hrishikesh <hrishikesh@gmail.com>
2025-06-18 23:10:02 +0530

    added policy entity class

--- src/main/java/com/riskmanagement/entity/PolicyEntity.java
+++ src/main/java/com/riskmanagement/entity/PolicyEntity.java
@@ -1,4 +1,28 @@
 package com.riskmanagement.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "policies")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class PolicyEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long policyId;
+
+    private String policyName;     // "annualRevenue", "hasLawsuits"
+    private String ruleType;      //  "MIN_VALUE", "MAX_VALUE", "EQUALS", "BOOLEAN_CHECK", "BETWEEN"
+    private String description;
+    private String expectedValue; // Used for EQUALS or BOOLEAN_CHECK (e.g., "false")
+    private Double minValue;      // Used for MIN_VALUE, BETWEEN
+    private Double maxValue;      // Used for MAX_VALUE, BETWEEN
+    private Boolean isActive;    //checking policy is active or not
 }
