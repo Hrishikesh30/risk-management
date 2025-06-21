c232a3fb09a1e7462f0726527e7b59a73aea5276
hrishikesh <hrishikesh@gmail.com>
2025-06-22 02:19:00 +0530

    added: application response DTO class

--- src/main/java/com/riskmanagement/dto/ApplicationResponseDTO.java
+++ src/main/java/com/riskmanagement/dto/ApplicationResponseDTO.java
@@ -1,4 +1,21 @@
 package com.riskmanagement.dto;
 
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
 public class ApplicationResponseDTO {
+    private Long applicationId;
+    private Long companyId;
+    private Long userId;
+    private String userName;
+    private String status;
+    private LocalDateTime submittedAt;
+
+
+//    private BigDecimal financialConditionRatio;
+//    private BigDecimal loanAmount;
+
 }
